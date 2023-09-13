949a4a187e0f52d13e88c9fadd3328b64040a29e
liam <lamtruong@panicbear.consulting>
2023-09-13 14:06:45 +0700

    init project

---
+++ js/components/header.js
@@ -0,0 +1,8 @@
+export default class HeaderComponent extends HTMLElement {
+    constructor() {
+        super();   
+    }
+}
+
+customElements.define('header-component', HeaderComponent);
+
---
+++ js/main.js
@@ -0,0 +1,11 @@
+import HeaderComponent from "./components/header";
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
